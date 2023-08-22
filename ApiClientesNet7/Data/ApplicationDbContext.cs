457e9a8cd0e61a0ff311730aee59af0c42abb8c7
jos-gabriel <jos.gabrie@gmail.com>
2023-08-21 23:57:17 -0600

    feat(Models): DbContext migrar User y Client

---
+++ ApiClientesNet7/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiClientesNet7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiClientesNet7.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Client> clients { get; set; }
+        public DbSet<User> users { get; set; }
+
+    }
+}
